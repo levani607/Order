43cfd4118824fa80de2c95615c36b2f60a178853
levan <levaniiantbelidze@gmail.com>
2024-08-28 15:17:28 +0400

    add eureka

--- src/main/java/org/example/orderservice/config/WebClientConfig.java
+++ src/main/java/org/example/orderservice/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package org.example.orderservice.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
      return    new RestTemplateBuilder()
                 .additionalInterceptors((request,body,execution)->{
